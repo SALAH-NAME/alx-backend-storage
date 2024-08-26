62dd453d668240f50ddb1f4ffda04a0e4546858d
Salah <Salah>
2024-08-26 20:21:16 +0100

    Nosql_fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,24 +8,23 @@ def log_stats():
     log_stats function
     """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
+    db = client.logs
+    collection = db.nginx
 
-    n_logs = collection.count_documents({})
-    print(f'{n_logs} logs')
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print('Methods:')
+    print("Methods:")
     for method in methods:
-        count = len(collection.find({"method": method}))
-        print(f'\tmethod {method}: {count}')
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
-    status_check = len(collection.find(
-        {"method": "GET", "path": "/status"}
-    ))
-
-    print(f'{status_check} status check')
-
+    status_check = collection.count_documents({
+        "method": "GET", 
+        "path": "/status"
+    })
+    print(f"{status_check} status check")
 
 if __name__ == "__main__":
-    """ main start """
     log_stats()
