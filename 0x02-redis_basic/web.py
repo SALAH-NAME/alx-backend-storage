37a33065e25298fbfc7aee6c4be64d7c822e0de2
Salah <Salah>
2024-08-28 11:20:09 +0100

    redis_basic

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" web.py """
+import redis
+import requests
+from typing import Callable
+from functools import wraps
+
+
+redis_client = redis.Redis()
+
+
+def count_requests(method: Callable) -> Callable:
+    """ count_requests """
+    @wraps(method)
+    def wrapper(url):
+        """ wrapper """
+        redis_client.incr(f"count:{url}")
+        cached_response = redis_client.get(f'result:{url}')
+        if cached_response:
+            return cached_response.decode('utf-8')
+        response = method(url)
+        redis_client.set(f'count:{url}', 0)
+        redis_client.setex(f'result:{url}', 10, response)
+        return response
+    return wrapper
+
+
+@count_requests
+def get_page(url: str) -> str:
+    """ get_page """
+    response = requests.get(url)
+    return response.text
